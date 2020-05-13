ddd79bcc98299e52806a9568ffef7f06d63c3d6b
el_psy_congroo <gaojingjingjing@outlook.com>
2020-05-13 15:34:39 +0900

    完成了分类参数的开发

--- src/plugin/element.js
+++ src/plugin/element.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 // import { Input } from 'element-ui'
 // import { Message } from 'element-ui'
 
-import { Button, Form, FormItem, Input ,Message, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader } from 'element-ui'
+import { Button, Form, FormItem, Input ,Message, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -36,3 +36,6 @@ Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
 Vue.use(Cascader)
+Vue.use(Alert)
+Vue.use(Tabs)
+Vue.use(TabPane)
